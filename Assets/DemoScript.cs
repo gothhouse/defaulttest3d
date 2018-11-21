98ffe369600da873aea5f5f43b52433188e61bf0
gothhouse <45217971+gothhouse@users.noreply.github.com>
2018-11-20 17:16:16 -0800

    Initial commit

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour { 
+
+    public Light myLight;
+
+    void Update() {
+        if (Input.GetKeyDown ("space")) {
+            myLight.enabled = !myLight.enabled;
+        }
+    }   
+}
